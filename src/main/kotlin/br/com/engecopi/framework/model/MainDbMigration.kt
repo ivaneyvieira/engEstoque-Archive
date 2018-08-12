ebf6bfa43d9a45590330e34bae898afd5e8d66af
ivaneyvieira <ivaneyvieira@pintos.com.br>
2018-08-12 10:07:01 -0300

    Versão de produtoção com migração dos dados corrigida

--- src/main/kotlin/br/com/engecopi/framework/model/MainDbMigration.kt
+++ src/main/kotlin/br/com/engecopi/framework/model/MainDbMigration.kt
@@ -11,7 +11,7 @@ object MainDbMigration {
    // System.setProperty("ddl.migration.generate", "true")
     
     System.setProperty("ddl.migration.name", "support end dating")
-    System.setProperty("ddl.migration.version", "V1.12")
+    //System.setProperty("ddl.migration.version", "V1.12")
     //System.setProperty("ddl.migration.pendingDropsFor", "1.10");
     val migration = DbMigration.create()
     migration.setStrictMode(false)
