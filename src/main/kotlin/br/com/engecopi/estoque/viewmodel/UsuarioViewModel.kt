1f91047f8b7d3b9f8e6ee14638aa06ae3a423af6
ivaneyvieira <ivaneyvieira@pintos.com.br>
2018-09-28 00:50:48 -0300

    Commit atual

--- src/main/kotlin/br/com/engecopi/estoque/viewmodel/UsuarioViewModel.kt
+++ src/main/kotlin/br/com/engecopi/estoque/viewmodel/UsuarioViewModel.kt
@@ -11,30 +11,27 @@ import br.com.engecopi.framework.viewmodel.IView
 
 class UsuarioViewModel(view: IView) : CrudViewModel<Usuario, QUsuario, UsuarioCrudVo>(view, UsuarioCrudVo::class) {
   val queryProduto get() = Produto.where()
-
+  
   fun findProduto(offset: Int, limit: Int): List<Produto> {
     return queryProduto
             .setFirstRow(offset)
             .setMaxRows(limit)
             .findList()
   }
-
+  
   fun countProduto(): Int {
     return queryProduto.findCount()
   }
-
+  
   override fun update(bean: UsuarioCrudVo) {
-    bean.entityVo?.let { usuario ->
-      val loginName = bean.loginName ?: ""
-      if (loginName.isNotBlank())
-        usuario.loginName = loginName
+    Usuario.findUsuario(bean.loginName ?: "")?.let { usuario ->
       usuario.loja = bean.loja
       usuario.impressora = bean.impressora ?: ""
       usuario.locais = bean.localizacaoes.toList()
       usuario.update()
     }
   }
-
+  
   override fun add(bean: UsuarioCrudVo) {
     val usuario = Usuario().apply {
       this.loginName = bean.loginName ?: ""
@@ -44,10 +41,10 @@ class UsuarioViewModel(view: IView) : CrudViewModel<Usuario, QUsuario, UsuarioCr
     }
     usuario.insert()
   }
-
+  
   override val query: QUsuario
     get() = Usuario.where()
-
+  
   override fun Usuario.toVO(): UsuarioCrudVo {
     val usuario = this
     return UsuarioCrudVo().apply {
@@ -58,15 +55,15 @@ class UsuarioViewModel(view: IView) : CrudViewModel<Usuario, QUsuario, UsuarioCr
       this.localizacaoes = usuario.locais.toHashSet()
     }
   }
-
+  
   override fun QUsuario.filterString(text: String): QUsuario {
     return loginName.contains(text)
   }
-
+  
   override fun delete(bean: UsuarioCrudVo) {
     Usuario.findUsuario(bean.loginName ?: "")?.delete()
   }
-
+  
   val lojas = execList { Loja.all() }
   val produtos: List<Produto>
     get() = Produto.all()
@@ -76,21 +73,26 @@ class UsuarioCrudVo : EntityVo<Usuario>() {
   override fun findEntity(): Usuario? {
     return Usuario.findUsuario(loginName)
   }
-
+  
   var loginName: String? = ""
   var impressora: String? = ""
+  
   var loja: Loja? = null
-    set(value) {
-      field = value
-      locaisLoja.clear()
-      val sets = ViewProdutoLoc.where().loja.id.eq(value?.id).findList()
-              .map { it.abreviacao }.distinct().toMutableSet()
-      locaisLoja.addAll(sets)
-    }
+  set(value) {
+    field = value
+    locaisLoja.clear()
+    val sets = ViewProdutoLoc.where().loja.id.eq(value?.id).findList()
+            .map { it.abreviacao }.distinct().toMutableSet()
+    locaisLoja.addAll(sets)
+  }
+  
   val nome
     get() = Usuario.nomeSaci(loginName ?: "")
+  
   var locaisLoja: MutableSet<String> = HashSet()
+  
   var localizacaoes: Set<String> = HashSet()
+  
   val localStr
     get() = localizacaoes.joinToString()
 }
