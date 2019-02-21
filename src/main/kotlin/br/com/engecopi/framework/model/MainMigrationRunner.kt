552c6d28756278fc6b249fcff1c573f1203fc085
ivaneyvieira <ivaneyvieira@gmail.com>
2019-02-21 08:30:30 -0300

    Atualização do gradle.build

--- src/main/kotlin/br/com/engecopi/framework/model/MainMigrationRunner.kt
+++ src/main/kotlin/br/com/engecopi/framework/model/MainMigrationRunner.kt
@@ -16,7 +16,11 @@ object MainMigrationRunner {
     val properties = Properties()
     properties.load(File(fileName).reader())
     val config = MigrationConfig()
-    config.load(properties)
+    config.dbUsername = properties.getProperty("datasource.db.username")
+    config.dbPassword = properties.getProperty("datasource.db.password")
+    config.dbDriver = properties.getProperty("datasource.db.databaseDriver")
+    config.dbUrl = properties.getProperty("datasource.db.databaseUrl")
+    config.isSkipChecksum = true
     val runner = MigrationRunner(config)
     runner.run()
   }
