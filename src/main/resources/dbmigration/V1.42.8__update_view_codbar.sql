59a4464a8a14f0bea72d380ee3df054e2e8ce188
ivaneyvieira <ivaneyvieira@gmail.com>
2019-02-25 16:44:50 -0300

    deploy 4.47

--- src/main/resources/dbmigration/V1.42.8__update_view_codbar.sql
+++ src/main/resources/dbmigration/V1.42.8__update_view_codbar.sql
@@ -7,10 +7,10 @@ REPLACE(
   )
   AS codbar,
   L.numero as storeno, N.numero, N.sequencia, substring_index(I.localizacao, '.',1) as abreviacao
-FROM engEstoque.itens_nota AS I
-  JOIN engEstoque.notas AS N
+FROM itens_nota AS I
+  JOIN notas AS N
   ON  N.id = I.nota_id
-  JOIN engEstoque.lojas AS L
+  JOIN lojas AS L
   ON  N.loja_id = L.id;
 
 CREATE OR REPLACE VIEW v_codigo_barra_entrega AS
@@ -20,7 +20,7 @@ replace(concat(L.numero,' ',N.numero,' ',N.sequencia,' ',substring_index(I.local
   '/',' ') AS codbar,
   L.numero as storeno, N.numero, N.sequencia, substring_index(I.localizacao,'.',1) AS abreviacao,
   P.codigo, P.grade, I.quantidade
-from engEstoque.itens_nota I
-join engEstoque.notas N on N.id = I.nota_id
-join engEstoque.lojas L on N.loja_id = L.id
-join engEstoque.produtos P on P.id = I.produto_id;
+from itens_nota I
+join notas N on N.id = I.nota_id
+join lojas L on N.loja_id = L.id
+join produtos P on P.id = I.produto_id;
